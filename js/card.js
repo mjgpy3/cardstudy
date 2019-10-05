935acb03ffd9c3f69a27d4a24893560f64971f41
Michael Gilliland <mjg.py3@gmail.com>
2019-10-04 23:55:41 -0400

    Find correct card side.

--- js/card.js
+++ js/card.js
@@ -1,7 +1,13 @@
 import decks from './decks.json';
 
-const renderCard = args => {
-  console.log(args);
+const renderCard = ({ deck, side, card }) => {
+  const [{ cards }] = decks.filter(({ name }) => name === deck);
+
+  const correctCard = cards[card];
+
+  const text = side === 'a' ? correctCard.sideA : correctCard.sideB;
+
+  console.log(text);
 };
 
 export { renderCard };
