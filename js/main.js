6569632e2223a623c92c6cc8ae85fb0b07bc1454
Michael Gilliland <mjg.py3@gmail.com>
2019-10-04 23:34:46 -0400

    Build deck list (fingers crossed).

--- js/main.js
+++ js/main.js
@@ -1,3 +1,62 @@
 import decks from './decks.json';
 
-console.log(JSON.stringify(decks, null, 2));
+const renderDeckList = () => {
+  const deckList = document.getElementById('deck-list');
+
+  const row = document.createElement('div');
+  row.className = 'row';
+
+  deckList.appendChild(row);
+
+  decks.forEach(deck => {
+    const col = document.createElement('div');
+    col.className = 'col s12 m3';
+
+    row.appendChild(col);
+
+    const card = document.createElement('div');
+    card.className = 'card';
+
+    col.appendChild(card);
+
+    const content = document.createElement('div');
+    content.className = 'card-content';
+
+    card.appendChild(content);
+
+    const action = document.createElement('div');
+    action.className = 'card-action';
+
+    card.appendChild(action);
+
+    const title = document.createElement('span');
+    title.className = 'card-title';
+
+    title.appendChild(
+      document.createTextNode(deck.name)
+    );
+
+    content.appendChild(title);
+
+    const body = document.createElement('p');
+
+    body.appendChild(
+      document.createTextNode(`Side A: ${deck.sideA}, Side B: ${deck.sideB}`)
+    );
+
+    content.appendChild(body);
+
+    const actionLink = document.createElement('a');
+    actionLink.href = `/deck=${deck.name}`;
+
+    action.appendChild(actionLink);
+
+    actionLink.appendChild(
+      document.createTextNode('Open Deck')
+    );
+  });
+};
+
+window.addEventListener('DOMContentLoaded', event => {
+  renderDeckList();
+});
